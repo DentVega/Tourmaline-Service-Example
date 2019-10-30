df3dc49fb1bd28a4808fb2ebffd2daa084c44f08
DentVega <dentvega6@gmail.com>
2019-10-30 09:40:39 -0400

    - constants

--- app/src/main/java/com/brianvega/tourmalineserviceexample/Constants.java
+++ app/src/main/java/com/brianvega/tourmalineserviceexample/Constants.java
@@ -2,8 +2,9 @@ package com.brianvega.tourmalineserviceexample;
 
 public interface Constants {
 
-    String API_KEY = "";
-    String EMAIL_USER = "";
+    String API_KEY = "api_key";
+    String EMAIL_USER = "email";
+    String PASSWORD = "password";
     String TEXT_NO_EMAIL_DEFINED = "No email defined";
     String NULL_VALUE = "null";
 
